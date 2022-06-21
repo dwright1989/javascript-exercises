7e789efc2df1f75821daaa04dc50ce422886dd95
Debbie Wright <debbiewright@live.co.uk>
2022-06-21 19:44:49 +0100

    Exercise 7 - tempConversions completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  let celTemp = (far -32)/1.8;
+  console.log("The cel temp is " + celTemp);
+  celTemp = Math.round(celTemp * 10) / 10
+  return celTemp;
+
 
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let farTemp = (cel * 1.8)+32;
+  console.log("The far temp is " + farTemp);
+  farTemp = Math.round(farTemp * 10) / 10
+  return farTemp;
 };
 
 // Do not edit below this line
