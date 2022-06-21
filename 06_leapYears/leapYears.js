a959936b609299c765dbe212b383b4949e06c998
Debbie Wright <debbiewright@live.co.uk>
2022-06-21 19:35:27 +0100

    Exercise 6 - leapyears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // check to see if it's divisale by four
+    if((year%4==0)){
+        // check it isn't divisable by 100 unless it's divisable by 400
+        if((year%100==0) && !(year%400==0)){
+            // this is not a leap year
+            return false;
+        }
 
+        return true;
+        
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
