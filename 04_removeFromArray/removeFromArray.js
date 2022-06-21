e69dee970acebd7c439ed6f3ed6a43b3516c6260
Debbie Wright <debbiewright@live.co.uk>
2022-06-21 19:07:12 +0100

    Exercise 4 - remove from Array compelted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function(...arg) {
+    // set the array 
+    let arr = arg[0];
+    // create a new array to be returned after manipulated
+    let newArr = [];
+
+    // loop through each array item
+    arr.forEach((item) => {
+        if(!arg.includes(item)){
+            newArr.push(item);
+        }
+
+    }
+
+    );
+
+    
+    return newArr;
 };
 
 // Do not edit below this line
