77897cae8799e97beb3070b4ba1d09781f8c1290
Debbie Wright <debbiewright@live.co.uk>
2022-06-21 18:14:51 +0100

    First two Javascript exercises (Hello World and Repeat String) completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(theString, num) {
+    let theNewString = "";
+    if(num<0){
+        return "ERROR";
+    }else{
+        for(let i=0; i<num; i++){
+            theNewString = theNewString + theString;
+        }
+        console.log(theNewString);
+        
+        return theNewString;
+    }
+    
 };
 
 // Do not edit below this line
