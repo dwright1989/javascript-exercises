e9a79bc9c0384d0de417db5a3dcc9ddec03e7006
Debbie Wright <debbiewright@live.co.uk>
2022-06-21 18:22:51 +0100

    Exercise 3 - reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(theString) {
+    let newString = "";
+    for(let i=0; i<theString.length; i++){
+        //console.log(theString.charAt((theString.length-1)-i));
+        newString = newString + theString.charAt((theString.length-1)-i);
+    }
+    return newString;
 };
 
 // Do not edit below this line
